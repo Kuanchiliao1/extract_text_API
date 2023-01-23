3f178d8af36927f96dfaaf765f05270c62c9e28f
kuanchiliao1 <kuanchiliao@gmail.com>
2023-01-23 11:10:41 -0500

    Add notes in Q/A format

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 import requests
 from bs4 import BeautifulSoup
 
+# Tells Flask to use current script(module) as the application
 app = Flask(__name__)
 
 # Define the endpoint and HTTP request method
@@ -25,3 +26,13 @@ def extract_text():
 
 if __name__ == "__main__":
   app.run()
+
+
+# Notes:
+# 1. What does @app.route allow us to do?
+# 2. What is app?
+# 3. On which line is `request` defined?
+
+# 1. The @app.route decorator is used to bind a function to a specific URL route. 
+# 2. Instance of the flask class and the main entry point for Flask application
+# 3. 1
